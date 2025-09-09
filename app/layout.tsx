29824dbdcb16f0984de34244b71dc41743ae254c
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-09-09 17:38:18 +0700

    Update font to mitr

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Prompt } from "next/font/google";
+import { Prompt, Mitr } from "next/font/google";
 import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
@@ -11,13 +11,18 @@ export const metadata: Metadata = {
   title: "RentMyRide - Car Rental Platform",
   description: "Find and rent the perfect car for your next adventure",
 };
-
-const prompt = Prompt({
-  variable: "--font-prompt",
+const mitr = Mitr({
+  variable: "--font-mitr",
   display: "swap",
   subsets: ["latin", "thai"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
+  weight: ["200", "300", "400", "500", "600", "700"],
+})
+// const prompt = Prompt({
+//   variable: "--font-prompt",
+//   display: "swap",
+//   subsets: ["latin", "thai"],
+//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+// });
 
 export default function RootLayout({
   children,
@@ -26,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${prompt.className} antialiased bg-white text-black`}>
+      <body className={`${mitr.className} antialiased bg-white text-black`}>
         {children}
       </body>
     </html>
