49122ad747a614ebeffb1a65f9973f884c7d89c2
BuFFaLoNoR <theerat.sc@gmail.com>
2025-09-10 20:37:52 +0700

    fix conflict

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -5,8 +5,7 @@ import { LogoutButton } from "./logout-button";
 
 export async function AuthButton() {
   const supabase = await createClient();
-
-  // You can also use getUser() which will be slower.
+  
   const { data } = await supabase.auth.getClaims();
 
   const user = data?.claims;
