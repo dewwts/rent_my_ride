7038b6d821b0ed4c30c440ffab29550445a1423c
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-09-28 10:43:55 +0700

    Implement getFirstname and GetRole

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -2,24 +2,21 @@ import Link from "next/link";
 import { LogInButton } from "./ui/login-button";
 import { createClient } from "@/lib/supabase/server";
 import { LogoutButton } from "./logout-button";
+import { getFirstname } from "@/lib/authServices";
 
 export async function AuthButton() {
   const supabase = await createClient();
   await supabase.auth.refreshSession();
   let displayName = null
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  if (user){
-    const {data: userInfo, error: err} = await supabase.from('user_info').select('u_firstname').eq('user_id',user.id).single()
-    if (userInfo){
-      displayName = userInfo.u_firstname
-    }
+  try{
+    const name = await getFirstname(supabase)
+    displayName = name;
+  }catch(err: unknown){
+    console.error(err)
   }
+  
   return displayName ? (
     <div className="flex items-center gap-3">
-
       <Link href="/dashboard"><span className="text-sm text-gray-600 font-medium">สวัสดี, {displayName}!</span></Link>
       <LogoutButton />
     </div>
--- lib/authServices.ts
+++ lib/authServices.ts
@@ -37,3 +37,32 @@ export const SignOut = async(supabase: SupabaseClient)=>{
     await supabase.auth.signOut()
     return true
 }
+
+export const getRole = async(supabase: SupabaseClient)=>{ 
+    const { data: { user } } = await supabase.auth.getUser();
+    if (!user) {
+      throw new Error("ไม่เจอผู้ใช้งานนี้ในฐานข้อมูล")
+    }
+    const {data : row ,error: getRoleError} =await supabase
+      .from("user_info")
+      .select("role")
+      .eq("user_id",user.id)
+      .maybeSingle();
+    if (getRoleError){
+        throw getRoleError
+    }
+    return row?.role
+}   
+export const getFirstname = async(supabase: SupabaseClient)=>{
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (user){
+      const {data: userInfo, error: err} = await supabase.from('user_info').select('u_firstname').eq('user_id',user.id).single()
+      if (userInfo){
+        return userInfo.u_firstname
+      }
+      throw new Error("ไม่เจอผู้ใช้งานนี้ในฐานข้อมูล")
+    }
+    // throw new Error("โปรดเข้าสู่ระบบก่อนใช้งาน")
+}
