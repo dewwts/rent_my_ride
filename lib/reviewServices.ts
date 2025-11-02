041bee7b981d1d71d24fdf0e9a6a42f51627be16
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-11-02 19:31:34 +0700

    Update example payload

--- lib/reviewServices.ts
+++ lib/reviewServices.ts
@@ -1,6 +1,25 @@
 import { Review } from "@/types/reviewInterface"
 import { SupabaseClient } from "@supabase/supabase-js"
 
+/*
+Submit a review for a renting
+How to use:
+    add parameter supabase client and payload of type Review
+returns true if successful, otherwise throws an error
+
+Review Schema is defined in types/reviewInterface.ts
+you will see ReviewPayload has extends Review and has renting_id
+for easier to maintainance and can use on get review for each car
+
+Example of payload:
+    paylaod = {
+        rating: 5,
+        message: "test",
+        reviewer_id: "user-123",
+        target_id: "user-456",
+        renting_id: "renting-789"
+    }
+*/
 export const submitReview = async(supabase: SupabaseClient, payload:Review)=>{
     const {error: insertError} = await supabase.from("reviews").insert(payload)
     if (insertError){
