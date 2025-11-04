5298f8a8ab604dda313bce20081d4919f32fbbe1
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-11-04 17:07:42 +0700

    Test bugging

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -46,7 +46,7 @@ export async function updateSession(request: NextRequest) {
   // with the Supabase client, your users may be randomly logged out.
   const { data } = await supabase.auth.getClaims();
   const user = data?.claims;
-
+  console.log(user);
   if (
     request.nextUrl.pathname !== "/" &&
     !user &&
