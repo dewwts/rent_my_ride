a66c932a0c4aebdf7cfdf4ddcb0ee82cc0ee57aa
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-11-04 18:38:57 +0700

    Update logout code

---
+++ test/playwright/auth.setup.ts
@@ -0,0 +1,16 @@
+import { test as setup, expect } from '@playwright/test';
+
+const authFile = 'playwright/.auth/user.json'
+const url = "https://rentmyride-mu.vercel.app/auth/login"
+setup('authenticate user and save strorage state', async({page})=>{
+    await page.goto(url)
+    await page.getByLabel("email").fill("pholapcondo11@gmail.com")
+    // หลักการคือ การใช้ getByLabel จะทำให้เราสามารถเข้าถึง input ที่มี label กำกับได้เลย
+    // โดยที่มันรู้ว่าเป็น input ไหนจากการจับคู่ label ที่ใช้ attribute htmlFor และ id ของ input ตรงกัน
+    await page.getByLabel("password").fill("123456")
+    await page.getByRole('button',{name:'เข้าสู่ระบบ'}).click()
+    // getByRole ก็จะ access ว่ามี button อะไรบ้างจากนั้นจะใช้ name ในการจับคู่โดยที่ name ก็คือ ข้อความที่แสดงในปุ่ม
+    await expect(page).toHaveURL("https://rentmyride-mu.vercel.app/dashboard")
+    await page.context().storageState({path:authFile})
+    // save state ไปที่ authFile
+})
--- test/playwright/authed/logout.spec.ts
+++ test/playwright/authed/logout.spec.ts
@@ -1,2 +1,21 @@
+import { test, expect } from '@playwright/test';
+const url = "https://rentmyride-mu.vercel.app/"
 
-const url = "https://rentmyride-mu.vercel.app/auth/login"
+test('Signout from authenticated state', async ({page}) => {
+    await page.goto(url)
+    await page.getByRole('button',{name:'ออกจากระบบ'}).click()
+    // หาเจอเพราะ element Button เป็น as child ของ button โดยมี name เป็น "ออกจากระบบ"
+    await expect(page).toHaveURL("https://rentmyride-mu.vercel.app/auth/login")
+    await expect(page.getByRole('link', { name: 'เข้าสู่ระบบ' })).toBeVisible();
+    // check ว่ากลับมาเป็น guest แล้วโดยการมองหาปุ่ม login
+})
+
+test('Access dashboard after signout should redirect to login', async ({page})=>{
+    await page.goto(url)
+    await page.getByRole('button',{name:'ออกจากระบบ'}).click()
+    // หาเจอเพราะ element Button เป็น as child ของ button โดยมี name เป็น "ออกจากระบบ"
+    await page.waitForURL("https://rentmyride-mu.vercel.app/auth/login")
+    // await expect(page.getByRole('link', { name: 'เข้าสู่ระบบ' })).toBeVisible();
+    await page.goto(url + "dashboard")
+    await expect(page).toHaveURL("https://rentmyride-mu.vercel.app/auth/login")
+})
