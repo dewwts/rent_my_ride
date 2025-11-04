a7367d031fe1afc42b5352a16e05f197d05be5ed
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-11-04 18:43:06 +0700

    Fix argument in getByLabel

--- test/playwright/auth.setup.ts
+++ test/playwright/auth.setup.ts
@@ -4,10 +4,10 @@ const authFile = 'playwright/.auth/user.json'
 const url = "https://rentmyride-mu.vercel.app/auth/login"
 setup('authenticate user and save strorage state', async({page})=>{
     await page.goto(url)
-    await page.getByLabel("email").fill("pholapcondo11@gmail.com")
+    await page.getByLabel("ที่อยู่อีเมล").fill("pholapcondo11@gmail.com")
     // หลักการคือ การใช้ getByLabel จะทำให้เราสามารถเข้าถึง input ที่มี label กำกับได้เลย โดยที่ argument ที่ใส่เข้าไปคือ ข้อความใน label
     // โดยที่มันรู้ว่าเป็น input ไหนจากการจับคู่ Label ที่ใช้ attribute htmlFor และ id ของ input ตรงกัน
-    await page.getByLabel("password").fill("123456")
+    await page.getByLabel("รหัสผ่าน").fill("123456")
     await page.getByRole('button',{name:'เข้าสู่ระบบ'}).click()
     // getByRole ก็จะ access ว่ามี button อะไรบ้างจากนั้นจะใช้ name ในการจับคู่โดยที่ name ก็คือ ข้อความที่แสดงในปุ่ม
     await expect(page).toHaveURL("https://rentmyride-mu.vercel.app/dashboard")
