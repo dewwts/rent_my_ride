554f1e345cf9744fdd5c921aecafc5249398725e
sunnieWL <weerapat.16141@gmail.com>
2025-11-10 21:17:17 +0700

    Fix quote notation (lint error)

--- components/cookie-consent.tsx
+++ components/cookie-consent.tsx
@@ -86,7 +86,7 @@ export function CookieConsent() {
 
           {/* Body Text */}
           <p className="text-black text-sm leading-relaxed">
-            We use cookies to enhance your browsing experience, serve personalized ads or content, and analyze our traffic. By clicking "Accept All", your consent to our use of cookies.
+            We use cookies to enhance your browsing experience, serve personalized ads or content, and analyze our traffic. By clicking &quot;Accept All&quot;, your consent to our use of cookies.
           </p>
 
           {/* Buttons */}
