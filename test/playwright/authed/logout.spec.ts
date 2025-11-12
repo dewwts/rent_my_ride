c28a2b9990a231bcb795797645c32f189f3a0d40
Peeradon <peeradonthienpongkasem@gmail.com>
2025-11-12 21:40:07 +0700

    fix every test accept consent

--- test/playwright/authed/logout.spec.ts
+++ test/playwright/authed/logout.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 const url = "https://rentmyride-mu.vercel.app/"
 
 test('Signout from authenticated state', async ({page}) => {
-    await page.goto(url)
+    await page.goto(url);
     await page.getByRole('button',{name:'ออกจากระบบ'}).click()
     // หาเจอเพราะ element Button เป็น as child ของ button โดยมี name เป็น "ออกจากระบบ"
     await expect(page.getByRole('button', { name: 'เข้าสู่ระบบ' })).toBeVisible({ timeout: 10000 });
